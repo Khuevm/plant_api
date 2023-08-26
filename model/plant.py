68068f4823fe6947a34dd8361330bb5ec35fcbc8
Khuevm <vngminhkhue@gmail.com>
2023-08-26 22:55:03 +0700

    Fix

--- model/plant.py
+++ model/plant.py
@@ -6,8 +6,3 @@ class PlantInfo:
         self.desc = desc
         self.image_link = image_link
 
-    def __init__(self, title, desc, image_link = "", care_guide):
-        self.title = title
-        self.desc = desc
-        self.image_link = image_link
-        self.careGuide = care_guide.__dict__
